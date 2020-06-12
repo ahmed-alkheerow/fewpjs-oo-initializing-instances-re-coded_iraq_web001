e6a5db3bcfc17b42523272713abf585ccc7f92e3
Ahmed Al-kheerow <a.alkheerow@gmail.com>
2020-06-12 20:19:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,13 @@ class Lunch {
     this.salad = salad;
     this.soup = soup;
     this.drink = drink;
-
-
+  }
+}
+class Dinner {
+  constructor(salad, soup, entree, dessert) {
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this._dessert = dessert;
   }
 }
